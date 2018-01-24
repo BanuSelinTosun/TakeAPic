62541a640e2ebb384e4db879788bf770d55d7a86
BanuSelinTosun <selin.tosun@gmail.com>
2018-01-24 00:33:03 -0800

    WebApp Local Update

--- TakeAPic/TakeAPic_local.py
+++ TakeAPic/TakeAPic_local.py
@@ -5,7 +5,8 @@ from werkzeug.utils import secure_filename    # Needed for image upload
 import GoogleFace_local
 import re
 import pandas as pd
-
+from StringIO import StringIO
+import base64
 # Flask location
 
 # Base path for project
@@ -25,24 +26,17 @@ PASSWORD = 'default'
 @app.route('/', methods = ['GET'])
 def index():
     method = flask.request.method
-
     return flask.render_template('index_local.html', title='Home')
 
 @app.route('/submissions', methods = ['GET', 'POST'])
 def submissions():
     uploaded_files = flask.request.files
+
     file_names = []
     for file in uploaded_files.getlist('file'):
         file_names.append(str(file.filename))
 
-    #print file_names
-
     directory_path = [TakeAPic_BASE_PATH + '/' + file_name for file_name in file_names]
-    #print directory_path
-    #
     df = GoogleFace_local.feed(directory_path)
-    #dictionary = df.T.to_dict().values()
-    #
-    # sample = [{'name':'1'},{'name':'2'}]
 
     return flask.render_template('submissions_local.html', name="Analysis", tables = [df.head(len(df)).to_html()])
